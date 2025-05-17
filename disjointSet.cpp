9407241006603702595c97364213ebc2db2b5fa8
asthas0108 <asthas0108@gmail.com>
2025-05-17 12:13:28 +0530

    add unionbySize

--- disjointSet.cpp
+++ disjointSet.cpp
@@ -4,11 +4,12 @@ using namespace std;
 class DisjointSet
 {
 
-    vector<int> parent, rank;
+    vector<int> parent, rank, size;
 
 public:
     DisjointSet(int n)
     {
+        size.resize(n + 1, 1);
         rank.resize(n + 1, 0);
         parent.resize(n + 1);
         for (int i = 0; i <= n; i++)
@@ -23,7 +24,7 @@ public:
         {
             return node;
         }
-        return parent[node] = findParent(parent[node]);
+        return parent[node] = findParent(parent[node]); // path compression O(n)
     }
 
     void unionByRank(int u, int v)
@@ -46,6 +47,26 @@ public:
         {
             parent[pv] = pu;
             rank[pu]++;
+        }
+    }
+
+    void unionBySize(int u, int v)
+    {
+        int pu = findParent(u);
+        int pv = findParent(v);
+        if (pu == pv)
+        {
+            return;
+        }
+        if (size[pu] < size[pv])
+        {
+            parent[pu] = pv;
+            size[pv] += size[pu];
+        }
+        else
+        {
+            parent[pv] = pu;
+            size[pu] += size[pv];
         }
     }
 };
