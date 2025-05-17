4a064f6ec2fa87641823ff44eb7ac65eb2a51ab5
asthas0108 <asthas0108@gmail.com>
2025-05-17 12:07:04 +0530

    disjoint set starter code

---
+++ disjointSet.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class DisjointSet
+{
+
+    vector<int> parent, rank;
+
+public:
+    DisjointSet(int n)
+    {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        for (int i = 0; i <= n; i++)
+        {
+            parent[i] = i;
+        }
+    }
+
+    int findParent(int node)
+    {
+        if (node == parent[node])
+        {
+            return node;
+        }
+        return parent[node] = findParent(parent[node]);
+    }
+
+    void unionByRank(int u, int v)
+    {
+        int pu = findParent(u);
+        int pv = findParent(v);
+        if (pu == pv)
+        {
+            return;
+        }
+        if (rank[pu] < rank[pv])
+        {
+            parent[pu] = pv;
+        }
+        else if (rank[pu] > rank[pv])
+        {
+            parent[pv] = pu;
+        }
+        else
+        {
+            parent[pv] = pu;
+            rank[pu]++;
+        }
+    }
+};
+
+int main()
+{
+    return 0;
+}
