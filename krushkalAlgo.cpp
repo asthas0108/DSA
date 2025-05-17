ddcef0df2de0cac74a624077ce3227ba9e15d0d0
asthas0108 <asthas0108@gmail.com>
2025-05-17 12:26:06 +0530

    add krushkal algo

---
+++ krushkalAlgo.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class DisjointSet
+{
+    vector<int> rank, parent, size;
+
+public:
+    DisjointSet(int n)
+    {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        for (int i = 0; i <= n; i++)
+        {
+            parent[i] = i;
+        }
+        size.resize(n + 1, 1);
+    }
+
+    int findParent(int node)
+    {
+        if (node == parent[node])
+            return node;
+
+        return parent[node] = findParent(parent[node]);
+    }
+
+    void unionByRank(int u, int v)
+    {
+        int pu = findParent(u);
+        int pv = findParent(v);
+
+        if (pu == pv)
+            return;
+
+        if (rank[pu] < rank[pv])
+        {
+            parent[pu] = pv;
+        }
+        else if (rank[pv] < rank[pu])
+        {
+            parent[pv] = pu;
+        }
+        else
+        {
+            parent[pv] = pu;
+            rank[pu]++;
+        }
+    }
+
+    void unionBySize(int u, int v)
+    {
+        int pu = findParent(u);
+        int pv = findParent(v);
+
+        if (pu == pv)
+            return;
+
+        if (size[pu] < size[pv])
+        {
+            parent[pu] = pv;
+            size[pv] += size[pu];
+        }
+        else
+        {
+            parent[pv] = pu;
+            size[pu] += size[pv];
+        }
+    }
+};
+class Solution
+{
+	public:
+	//Function to find sum of weights of edges of the Minimum Spanning Tree.
+    int spanningTree(int V, vector<vector<int>> adj[])
+    {
+        vector<pair<int, pair<int, int>>>edges;
+        
+        for(int i=0;i<V;i++){
+            for(auto it: adj[i]){
+                int adjNode = it[0];
+                int wt = it[1];
+                int node = i;
+                
+                edges.push_back({wt, {node, adjNode}});
+            }
+        }
+        
+        DisjointSet ds(V);
+        
+        sort(edges.begin(), edges.end());
+        
+        int ans=0;
+        
+        for(auto it: edges){
+            int w = it.first;
+            int u = it.second.first;
+            int v = it.second.second;
+            
+            if(ds.findParent(u) != ds.findParent(v)){
+                ans+=w;
+                ds.unionBySize(u, v);
+            }
+        }
+        
+        return ans;
+    }
+};
